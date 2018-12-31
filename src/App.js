64ab8004651703190edd9c12af7a0d71d9b8e6ab
codypearce <codyapearce@gmail.com>
2018-12-30 16:15:07 -0800

    set up eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Other } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
