45c5eb3072e530afec6950a2a8ca0e9cceec80c8
codypearce <codyapearce@gmail.com>
2019-01-01 12:30:34 -0800

    add align, coolor, gutterBottom as options to all text

--- __tests__/Components/Typography/BodyText.test.js
+++ __tests__/Components/Typography/BodyText.test.js
@@ -17,3 +17,14 @@ test('it renders BodyText One when a value isnt 1 or 2', () => {
   const tree = renderer.create(<BodyText type={3}>BodyText</BodyText>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('it renders BodyText One with other props', () => {
+  const tree = renderer
+    .create(
+      <BodyText type={3} align="center" gutterBottom={true}>
+        BodyText
+      </BodyText>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/Components/Typography/BaseText.js
+++ src/Components/Typography/BaseText.js
@@ -13,16 +13,29 @@ export default class BaseText extends Component {
     style: PropTypes.object,
     children: PropTypes.node,
     typographyStyles: PropTypes.object,
+    align: PropTypes.string,
+    color: PropTypes.string,
+    gutterBottom: PropTypes.bool,
   };
 
   render() {
-    const { style, typographyStyles, children } = this.props;
+    const {
+      style,
+      typographyStyles,
+      align,
+      color,
+      gutterBottom,
+      children,
+    } = this.props;
 
     return (
       <Text
         style={{
           ...styles.default,
           ...typographyStyles,
+          textAlign: align ? align : 'left',
+          color: color ? color : 'rgba(0,0,0, 87)',
+          marginBottom: gutterBottom ? 10 : 0,
           ...style,
         }}
         {...this.props}>
