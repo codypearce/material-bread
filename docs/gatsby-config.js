168233e048c0b84b3b95ef1697ddb9b5c326a7da
codypearce <codyapearce@gmail.com>
2019-03-03 23:18:08 -0800

    add remark, filesystem, update package.json, readme

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Material Bread | React Material Design Components`,
+    description: `React and React Native Components that implement Material Design Components. Designed to be highly customizable`,
+    author: `codypearce`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
