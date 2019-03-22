1f9918b988f18624f24ab0769afc09cc301d2ec7
codypearce <codyapearce@gmail.com>
2019-03-21 23:01:58 -0700

    create doc component index

---
+++ docs/src/components/index.js
@@ -0,0 +1,41 @@
+// COMPONENTS
+export { default as P } from './P';
+export { default as Link } from './Link';
+export { default as PageTitle } from './PageTitle';
+export { default as Section } from './Section';
+export { default as SectionHeader } from './SectionHeader';
+export { default as CodeInline } from './CodeInline';
+export { default as CodeBlock } from './CodeBlock';
+
+// COLOR PALLETE
+export { default as ColorPalette } from './ColorPalette/ColorPalette';
+
+// COMOPNENT PAGE
+export {
+  default as ComponentDescription,
+} from './ComponentPage/ComponentDescription';
+export { default as ComponentHeader } from './ComponentPage/ComponentHeader';
+export {
+  default as ComponentPageLayout,
+} from './ComponentPage/ComponentPageLayout';
+export {
+  default as ComponentSubtitle,
+} from './ComponentPage/ComponentSubtitle';
+
+// ICON DIRECTORY
+export { default as IconDirectory } from './IconDirectory/IconDirectory';
+
+// LAYOUT
+export { default as Header } from './Layout/Header';
+export { default as Layout } from './Layout/Layout';
+export { default as PageLayout } from './Layout/PageLayout';
+
+// LIVE EDIT
+export { default as LiveEdit } from './LiveEdit/LiveEdit';
+export { default as LiveEditToolTip } from './LiveEdit/LiveEditToolTip';
+
+// PROPSTABLE
+export { default as PropsTable } from './PropsTable';
+
+// SIDESCROLL
+export { default as SideScrollMenu } from './SideScrollMenu';
