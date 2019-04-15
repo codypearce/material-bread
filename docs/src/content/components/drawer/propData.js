258bc15b7609bdad7f6e346cbc36b02b85360756
codypearce <codyapearce@gmail.com>
2019-04-15 00:08:29 -0700

    Update drawer props

--- docs/src/content/components/drawer/propData.js
+++ docs/src/content/components/drawer/propData.js
@@ -1,13 +1,58 @@
 import { createTableData } from '../../../utils/createPropData';
 const propData = [
-  ['containerStyle', 'Styles outer container element', 'object', ''],
   [
-    'contentContainerStyle',
-    'Styles content within scroll element',
-    'object',
+    'animationTime',
+    'How long the drawer animation should be, in ms',
+    'number',
+    '200',
+  ],
+  [
+    'appbar',
+    'Render appbar above drawer and page content for clipping purposes',
+    'node',
     '',
   ],
+  ['contentContainerStyle', 'Styles wrapper around page content', 'object', ''],
+  ['drawerContent', 'Components displayed inside the drawer', 'node', ''],
+  ['drawerStyle', 'Styles drawer', 'object', ''],
+  ['onClose', 'Callback when clicking outside of drawer', 'func', ''],
+  [
+    'pageHeight',
+    'Override calculated pageHeight, useful for demos as shown',
+    'number',
+    'ScreenHeight',
+  ],
+
+  [
+    'pageWidth',
+    'Override calculated pageWidth, useful pages that do not take up full space',
+    'number',
+    'ScreenWidth',
+  ],
+  ['open', 'Whether drawer is shown or hidden', 'bool', 'false'],
   ['style', 'Styles scroll element', 'object', ''],
+  ['scrim', 'Whether scrim is shown', 'bool', 'true'],
+  ['scrimColor', 'Color of scrim', 'object', 'black'],
+  ['scrimOpacity', 'The ending opacity for the scim', 'number', '.4'],
+  [
+    'type',
+    'Determines the type of drawer from modal, push, and permanenent',
+    'string',
+    'modal',
+  ],
+
+  [
+    'width',
+    'Specific drawer width in pixels, will overrid widthPercentage',
+    'number',
+    '240',
+  ],
+  [
+    'widthPercentage',
+    'Percentage of pageWidth the drawer should take up',
+    '0-1',
+    '.40',
+  ],
 ];
 
 export default createTableData(propData);
