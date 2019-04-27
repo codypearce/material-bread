b3237afeea218488acf75efd5cb69e5c5772ebe2
codypearce <codyapearce@gmail.com>
2019-04-27 02:33:05 -0700

    40 add more icon demos

--- src/Components/Checkbox/Checkbox.stories.js
+++ src/Components/Checkbox/Checkbox.stories.js
@@ -191,11 +191,63 @@ export default storiesOf('Components|Checkbox', module)
             style={{
               flexDirection: 'row',
               alignItems: 'center',
-              justifyContent: 'space-between',
+              flexWrap: 'wrap',
+            }}>
+            <Checkbox
+              checked={state.checkedOnePrime}
+              icon={'favorite-border'}
+              checkedIcon={'favorite'}
+              checkboxColor={'#F44336'}
+              onPress={() =>
+                store.set({ checkedOnePrime: !state.checkedOnePrime })
+              }
+            />
+            <Checkbox
+              rippleMatchesCheckbox
+              checkboxColor={'#009688'}
+              checked={state.checkedTwoPrime}
+              icon={'add-circle-outline'}
+              checkedIcon={'add-circle'}
+              onPress={() =>
+                store.set({ checkedTwoPrime: !state.checkedTwoPrime })
+              }
+            />
+            <Checkbox
+              rippleMatchesCheckbox
+              checkboxColor={'#E91E63'}
+              icon={'bookmark-border'}
+              checkedIcon={'bookmark'}
+              checked={state.checkedThreePrime}
+              onPress={() =>
+                store.set({ checkedThreePrime: !state.checkedThreePrime })
+              }
+            />
+            <Checkbox
+              icon={'folder-open'}
+              checkedIcon={'folder'}
+              checkboxColor={'#F44336'}
+              checked={state.checkedFourPrime}
+              onPress={() =>
+                store.set({ checkedFourPrime: !state.checkedFourPrime })
+              }
+            />
+          </View>
+        )}
+      </State>
+
+      <State store={store} style={{ flex: 1 }}>
+        {state => (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
               flexWrap: 'wrap',
             }}>
             <Checkbox
               checked={state.checkedOne}
+              icon={'favorite-border'}
+              checkedIcon={'favorite'}
+              checkboxColor={'#F44336'}
               onPress={() => store.set({ checkedOne: !state.checkedOne })}
             />
             <Checkbox
@@ -215,15 +267,111 @@ export default storiesOf('Components|Checkbox', module)
               onPress={() => store.set({ checkedThree: !state.checkedThree })}
             />
             <Checkbox
-              indeterminate
+              icon={'folder-open'}
+              checkedIcon={'folder'}
               checkboxColor={'#F44336'}
               checked={state.checkedFour}
               onPress={() => store.set({ checkedFour: !state.checkedFour })}
             />
+          </View>
+        )}
+      </State>
+
+      <State store={store} style={{ flex: 1 }}>
+        {state => (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}>
             <Checkbox
-              checked={state.checkedFive}
+              checked={state.checkedOnePrime}
+              icon={'favorite-border'}
+              checkedIcon={'favorite'}
+              checkboxColor={'#F44336'}
+              unCheckedColor={'#F44336'}
+              onPress={() =>
+                store.set({ checkedOnePrime: !state.checkedOnePrime })
+              }
+            />
+            <Checkbox
+              rippleMatchesCheckbox
+              checkboxColor={'#009688'}
+              unCheckedColor={'#009688'}
+              checked={state.checkedTwoPrime}
+              icon={'add-circle-outline'}
+              checkedIcon={'add-circle'}
+              onPress={() =>
+                store.set({ checkedTwoPrime: !state.checkedTwoPrime })
+              }
+            />
+            <Checkbox
+              rippleMatchesCheckbox
+              checkboxColor={'#E91E63'}
+              unCheckedColor={'#E91E63'}
+              icon={'bookmark-border'}
+              checkedIcon={'bookmark'}
+              checked={state.checkedThreePrime}
+              onPress={() =>
+                store.set({ checkedThreePrime: !state.checkedThreePrime })
+              }
+            />
+            <Checkbox
+              checkboxColor={'#F44336'}
+              unCheckedColor={'#F44336'}
+              icon={'folder-open'}
+              checkedIcon={'folder'}
+              checked={state.checkedFourPrime}
+              onPress={() =>
+                store.set({ checkedFourPrime: !state.checkedFourPrime })
+              }
+            />
+          </View>
+        )}
+      </State>
+
+      <State store={store} style={{ flex: 1 }}>
+        {state => (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}>
+            <Checkbox
               disabled
-              onPress={() => store.set({ checkedFive: !state.checkedFive })}
+              checked={state.checkedOne}
+              icon={'favorite-border'}
+              checkedIcon={'favorite'}
+              checkboxColor={'#F44336'}
+              onPress={() => store.set({ checkedOne: !state.checkedOne })}
+            />
+            <Checkbox
+              disabled
+              rippleMatchesCheckbox
+              checkboxColor={'#009688'}
+              checked={state.checkedTwo}
+              icon={'add-circle-outline'}
+              checkedIcon={'add-circle'}
+              onPress={() => store.set({ checkedTwo: !state.checkedTwo })}
+            />
+            <Checkbox
+              disabled
+              rippleMatchesCheckbox
+              checkboxColor={'#E91E63'}
+              icon={'bookmark-border'}
+              checkedIcon={'bookmark'}
+              checked={state.checkedThree}
+              onPress={() => store.set({ checkedThree: !state.checkedThree })}
+            />
+            <Checkbox
+              disabled
+              icon={'folder-open'}
+              checkedIcon={'folder'}
+              checkboxColor={'#F44336'}
+              checked={state.checkedFour}
+              onPress={() => store.set({ checkedFour: !state.checkedFour })}
             />
           </View>
         )}
