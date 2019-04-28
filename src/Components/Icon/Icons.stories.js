bdacf63aa93ee0c2a67a06398506ef1243af6be3
codypearce <codyapearce@gmail.com>
2019-04-28 01:32:13 -0700

    78 add more icon demos

--- src/Components/Icon/Icons.stories.js
+++ src/Components/Icon/Icons.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 
 import { Icon, Badge } from '../../';
 import Header from '../../storybook/components/Header';
@@ -11,14 +12,34 @@ export default storiesOf('Components|Icons', module)
     <Container>
       <Header title={'Icon Component'} />
 
-      <Icon name="alarm" size={32} />
-      <Icon name="face" size={32} />
-      <Icon name="arrow-back" size={32} />
-      <Badge
-        style={{ position: 'absolute', top: 0, right: 0 }}
-        size={14}
-        badgeContent={4}>
-        <Icon name="favorite" size={32} />
-      </Badge>
+      <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+        <Icon name="alarm" size={32} />
+        <Icon name="face" size={32} />
+        <Icon name="arrow-back" size={32} />
+        <Badge
+          style={{ position: 'absolute', top: 0, right: 0 }}
+          size={14}
+          badgeContent={4}>
+          <Icon name="favorite" size={32} />
+        </Badge>
+      </View>
+      <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+        <Icon name="adb" size={12} color={'#F44336'} />
+        <Icon name="album" size={24} color={'#E91E63'} />
+        <Icon name="battery-full" size={32} color={'#9C27B0'} />
+        <Badge
+          style={{ position: 'absolute', top: 0, right: 0 }}
+          size={14}
+          badgeContent={4}>
+          <Icon name="camera" size={48} color={'#009688'} />
+        </Badge>
+      </View>
+      <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+        <Icon name="create" size={24} color={'#00BCD4'} />
+        <Icon name="dashboard" size={32} color={'#607D8B'} />
+        <Icon name="directions-walk" size={48} color={'#8BC34A'} />
+
+        <Icon name="flash-on" size={64} color={'#FFEB3B'} />
+      </View>
     </Container>
   ));
