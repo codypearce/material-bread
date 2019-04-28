9e4400f1396d46a0ed535dbcd833f20d70ce1cce
codypearce <codyapearce@gmail.com>
2019-04-27 21:04:25 -0700

    47 add demos to docs

--- docs/src/content/components/dialog/index.js
+++ docs/src/content/components/dialog/index.js
@@ -3,8 +3,15 @@ import ComponentPageLayout from '../../../components/ComponentPage/ComponentPage
 import MainDemo from './MainDemo';
 import Usage from './Usage';
 import Props from './Props';
+import Demos from './Demos';
 
-const sections = [{ name: 'Component' }, { name: 'Usage' }, { name: 'Props' }];
+const sections = [
+  { name: 'Component' },
+  { name: 'Usage' },
+  { name: 'Props' },
+  { name: 'alert', sub: true },
+  { name: 'simple', sub: true },
+];
 
 export default class DialogPage extends Component {
   render() {
@@ -20,6 +27,7 @@ export default class DialogPage extends Component {
           <MainDemo pageHref={'/components/dialog'} />
           <Usage pageHref={'/components/dialog'} />
           <Props pageHref={'/components/dialog'} />
+          <Demos pageHref={'/components/chip'} />
         </ComponentPageLayout>
       </div>
     );
