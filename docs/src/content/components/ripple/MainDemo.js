9ef9403e6a660e9f9edbe26f13f1a113fa8813b2
codypearce <codyapearce@gmail.com>
2019-04-28 21:42:02 -0700

    91 add more demos to docs for ripple

--- docs/src/content/components/ripple/MainDemo.js
+++ docs/src/content/components/ripple/MainDemo.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { ComponentMainDemo } from '@components';
-import { Ripple } from '../../../../../src/index';
+import { ComponentMainDemo, CodeInline } from '@components';
+import { Ripple, Card } from '../../../../../src/index';
 
 export const code = `class Demo extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-
-    }
   }
   render() {
     return (
-      <View>
+      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
         <Ripple
           rippleColor={'blue'}
           style={{
@@ -26,6 +23,18 @@ export const code = `class Demo extends React.Component {
           }}>
           <Text>Click Me</Text>
         </Ripple>
+        <Ripple style={{ width: 200, height: 200, marginLeft: 40 }}>
+          <Card
+            shadow={4}
+            style={{
+              width: 200,
+              height: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>Click Me</Text>
+          </Card>
+        </Ripple>
       </View>
     );
   }
@@ -34,9 +43,18 @@ export const code = `class Demo extends React.Component {
 const MainDemo = pageHref => (
   <ComponentMainDemo
     pageHref={pageHref}
-    description={`The Ripple component is a base component that can be used to add a ripple animation on touch. There are many options to customize the ripple component further.`}
+    description={
+      <div>
+        <CodeInline code="Ripple" type="element" /> is a base component that can
+        be used to add a ripple animation to anything on touch. You can change{' '}
+        <CodeInline code="rippleDuration" type="prop" />,{' '}
+        <CodeInline code="rippleColor" type="prop" />,{' '}
+        <CodeInline code="rippleContainerBorderRadius" type="prop" />, and much
+        more.
+      </div>
+    }
     code={code}
-    scope={{ View, Text, Ripple }}
+    scope={{ View, Text, Ripple, Card }}
   />
 );
 export default MainDemo;
