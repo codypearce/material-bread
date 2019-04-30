229ccfe6dbd77f535423190b100271862b298626
codypearce <codyapearce@gmail.com>
2019-04-30 03:15:13 -0700

    83 add icon, media, and demo

--- src/Components/List/List.stories.js
+++ src/Components/List/List.stories.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { List, ListItem, ListExpanded } from '../../';
+import { List, ListItem, ListExpanded, Icon } from '../../';
 import Header from '../../storybook/components/Header';
 import Container from '../../storybook/components/Container';
 import { storiesOf } from '../../storybook/helpers/storiesOf';
 
 export default storiesOf('Components|List', module)
   .addParameters({ jest: ['List'] })
-  .add('Text List', () => (
+  .add('Text', () => (
     <Container>
-      <Header title={'TextList'} />
+      <Header title={'Text List'} />
 
-      <List style={{ width: 400 }}>
+      <List style={{ width: 300 }}>
         <ListItem text={'Attractions'} />
         <ListItem text={'Dining'} />
         <ListItem text={'Education'} />
@@ -19,6 +19,33 @@ export default storiesOf('Components|List', module)
         <ListItem text={'Family'} />
         <ListItem text={'Office'} />
         <ListItem text={'Promotions'} />
+      </List>
+    </Container>
+  ))
+  .add('with icon', () => (
+    <Container>
+      <Header title={'Text and Icon List'} />
+
+      <List style={{ width: 300 }}>
+        <ListItem
+          text={'Attractions'}
+          icon={<Icon name={'local-movies'} size={24} />}
+        />
+        <ListItem
+          text={'Dining'}
+          icon={<Icon name={'local-dining'} size={24} />}
+        />
+        <ListItem text={'Education'} icon={<Icon name={'edit'} size={24} />} />
+        <ListItem text={'Health'} icon={<Icon name={'favorite'} size={24} />} />
+        <ListItem text={'Family'} icon={<Icon name={'group'} size={24} />} />
+        <ListItem
+          text={'Office'}
+          icon={<Icon name={'content-cut'} size={24} />}
+        />
+        <ListItem
+          text={'Promotions'}
+          icon={<Icon name={'loyalty'} size={24} />}
+        />
       </List>
     </Container>
   ));
--- src/Components/List/ListItem/ListItem.js
+++ src/Components/List/ListItem/ListItem.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { View } from 'react-native';
+
 import withTheme from '../../../Theme/withTheme';
 import { BodyText, Ripple } from '../../..';
 import styles from './ListItem.styles';
@@ -12,6 +14,8 @@ class ListItem extends Component {
     disabled: PropTypes.bool,
     selected: PropTypes.bool,
     text: PropTypes.string,
+    media: PropTypes.node,
+    icon: PropTypes.node,
   };
 
   _renderText() {
@@ -20,8 +24,29 @@ class ListItem extends Component {
     return <BodyText style={styles.listText}>{text}</BodyText>;
   }
 
+  _renderIcon() {
+    const { icon } = this.props;
+    console.log(icon);
+    return React.cloneElement(icon, {
+      size: icon.props.size ? icon.props.size : 16,
+      color: icon.props.color ? icon.props.color : '#6e6e6e',
+    });
+  }
+
   render() {
-    const { style, onPress, disabled, selected, children } = this.props;
+    const {
+      style,
+      onPress,
+      disabled,
+      selected,
+      children,
+      media,
+      icon,
+    } = this.props;
+
+    let contentMargin = media ? 16 : 0;
+    if (icon) contentMargin = 32;
+
     return (
       <Ripple
         onAnimationEnd={onPress}
@@ -42,7 +67,11 @@ class ListItem extends Component {
           },
           style,
         ]}>
-        {children ? children : this._renderText()}
+        {icon ? this._renderIcon() : null}
+        {media ? media : null}
+        <View style={{ marginLeft: contentMargin }}>
+          {children ? children : this._renderText()}
+        </View>
       </Ripple>
     );
   }
