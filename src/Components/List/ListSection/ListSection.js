0000f11eb453a7a330fb6352dc1e837ddd4401be
codypearce <codyapearce@gmail.com>
2019-04-30 03:56:40 -0700

    83 listsection label, divider, inset demos

--- src/Components/List/ListSection/ListSection.js
+++ src/Components/List/ListSection/ListSection.js
@@ -13,17 +13,27 @@ class ListSection extends Component {
     label: PropTypes.string,
     topDivider: PropTypes.bool,
     bottomDivider: PropTypes.bool,
+    inset: PropTypes.bool,
   };
 
   render() {
-    const { children, label, topDivider, bottomDivider } = this.props;
+    const { children, label, topDivider, bottomDivider, inset } = this.props;
 
     return (
       <View style={[styles.drawerSection, { paddingTop: label ? 8 : 0 }]}>
         {topDivider ? <Divider /> : null}
         {label ? <Text style={styles.label}>{label}</Text> : null}
         <View style={styles.content}>{children}</View>
-        {bottomDivider ? <Divider /> : null}
+        {bottomDivider ? (
+          <Divider
+            style={{
+              width: inset ? 'calc(100% - 72px)' : '100%',
+              marginLeft: inset ? 72 : 0,
+              marginTop: 0,
+              marginBottom: 0,
+            }}
+          />
+        ) : null}
       </View>
     );
   }
--- src/Components/List/ListSection/ListSection.stories.js
+++ src/Components/List/ListSection/ListSection.stories.js
@@ -1,60 +1,126 @@
 import React from 'react';
 
-import { List, ListItem, Icon, Avatar, Checkbox, IconButton } from '../../../';
+import { List, ListItem, Icon, ListSection } from '../../../';
 import Header from '../../../storybook/components/Header';
 import Container from '../../../storybook/components/Container';
 import { storiesOf } from '../../../storybook/helpers/storiesOf';
-import { State, Store } from '@sambego/storybook-state';
-
-const store = new Store({
-  checkedOne: false,
-  checkedTwo: false,
-  checkedThree: false,
-  checkedFour: false,
-  checkedFive: false,
-});
 
 export default storiesOf('Components|List/ListSection', module)
   .addParameters({ jest: ['List'] })
-  .add('with text', () => (
+  .add('with label', () => (
     <Container>
-      <Header title={'Text List'} />
+      <Header title={'Label List'} />
 
       <List style={{ width: 300 }}>
-        <ListItem text={'Attractions'} />
-        <ListItem text={'Dining'} />
-        <ListItem text={'Education'} />
-        <ListItem text={'Health'} />
-        <ListItem text={'Family'} />
-        <ListItem text={'Office'} />
-        <ListItem text={'Promotions'} />
+        <ListSection label={'Entertainment'}>
+          <ListItem
+            text={'Attractions'}
+            icon={<Icon name={'local-movies'} size={24} />}
+          />
+          <ListItem
+            text={'Dining'}
+            icon={<Icon name={'local-dining'} size={24} />}
+          />
+        </ListSection>
+        <ListSection label={'Home'}>
+          <ListItem
+            text={'Health'}
+            icon={<Icon name={'favorite'} size={24} />}
+          />
+          <ListItem text={'Family'} icon={<Icon name={'group'} size={24} />} />
+        </ListSection>
+        <ListSection label={'Job'}>
+          <ListItem
+            text={'Education'}
+            icon={<Icon name={'edit'} size={24} />}
+          />
+          <ListItem
+            text={'Office'}
+            icon={<Icon name={'content-cut'} size={24} />}
+          />
+          <ListItem
+            text={'Promotions'}
+            icon={<Icon name={'loyalty'} size={24} />}
+          />
+        </ListSection>
       </List>
     </Container>
   ))
-  .add('with icon', () => (
+  .add('with divider', () => (
     <Container>
-      <Header title={'Text and Icon List'} />
+      <Header title={'Label List'} />
 
       <List style={{ width: 300 }}>
-        <ListItem
-          text={'Attractions'}
-          icon={<Icon name={'local-movies'} size={24} />}
-        />
-        <ListItem
-          text={'Dining'}
-          icon={<Icon name={'local-dining'} size={24} />}
-        />
-        <ListItem text={'Education'} icon={<Icon name={'edit'} size={24} />} />
-        <ListItem text={'Health'} icon={<Icon name={'favorite'} size={24} />} />
-        <ListItem text={'Family'} icon={<Icon name={'group'} size={24} />} />
-        <ListItem
-          text={'Office'}
-          icon={<Icon name={'content-cut'} size={24} />}
-        />
-        <ListItem
-          text={'Promotions'}
-          icon={<Icon name={'loyalty'} size={24} />}
-        />
+        <ListSection bottomDivider>
+          <ListItem
+            text={'Attractions'}
+            icon={<Icon name={'local-movies'} size={24} />}
+          />
+          <ListItem
+            text={'Dining'}
+            icon={<Icon name={'local-dining'} size={24} />}
+          />
+        </ListSection>
+        <ListSection bottomDivider>
+          <ListItem
+            text={'Health'}
+            icon={<Icon name={'favorite'} size={24} />}
+          />
+          <ListItem text={'Family'} icon={<Icon name={'group'} size={24} />} />
+        </ListSection>
+        <ListSection>
+          <ListItem
+            text={'Education'}
+            icon={<Icon name={'edit'} size={24} />}
+          />
+          <ListItem
+            text={'Office'}
+            icon={<Icon name={'content-cut'} size={24} />}
+          />
+          <ListItem
+            text={'Promotions'}
+            icon={<Icon name={'loyalty'} size={24} />}
+          />
+        </ListSection>
+      </List>
+    </Container>
+  ))
+  .add('with inset divider', () => (
+    <Container>
+      <Header title={'Label List'} />
+
+      <List style={{ width: 300 }}>
+        <ListSection inset bottomDivider>
+          <ListItem
+            text={'Attractions'}
+            icon={<Icon name={'local-movies'} size={24} />}
+          />
+          <ListItem
+            text={'Dining'}
+            icon={<Icon name={'local-dining'} size={24} />}
+          />
+        </ListSection>
+        <ListSection inset bottomDivider>
+          <ListItem
+            text={'Health'}
+            icon={<Icon name={'favorite'} size={24} />}
+          />
+          <ListItem text={'Family'} icon={<Icon name={'group'} size={24} />} />
+        </ListSection>
+        <ListSection inset>
+          <ListItem
+            text={'Education'}
+            icon={<Icon name={'edit'} size={24} />}
+          />
+          <ListItem
+            text={'Office'}
+            icon={<Icon name={'content-cut'} size={24} />}
+          />
+          <ListItem
+            text={'Promotions'}
+            icon={<Icon name={'loyalty'} size={24} />}
+          />
+        </ListSection>
       </List>
     </Container>
   ));
