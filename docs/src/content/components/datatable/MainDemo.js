5554fe09980f980a0dcfd6cb7f377f4b1293fb7f
codypearce <codyapearce@gmail.com>
2019-05-06 03:14:23 -0700

    54 update main demo for datatable

--- docs/src/content/components/datatable/MainDemo.js
+++ docs/src/content/components/datatable/MainDemo.js
@@ -13,34 +13,61 @@ export const code = `class Table extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
     }
   }
   render() {
     return (
       <DataTable>
-          <DataTableRow>
+          <DataTableHeader
+            title={'Nutrition'}
+            rightActions={[
+              {name: 'filter-list'},
+              {name: 'more-vert'},
+            ]} 
+          />
+          <DataTableRow  showCheckbox>
             <DataTableCell text={'Desert'} type={'header'} borderRight flex={2} />
-            <DataTableCell text={'Calories'}  type={'header'}  right />
+            <DataTableCell
+              text={'Calories'}
+              type={'header'}  right
+            />
             <DataTableCell text={'Fat (g)'}  type={'header'}  right />
             <DataTableCell text={'Carbs (g)'}  type={'header'}  right />
             <DataTableCell text={'Protein (g)'}  type={'header'}  right />
           </DataTableRow>
-          <DataTableRow>
-              <DataTableCell text={'Frozen yogurt'} borderRight flex={2} />
-              <DataTableCell text={'159'} right />
-              <DataTableCell text={'6.0'} right />
-              <DataTableCell text={'24'} right />
-              <DataTableCell text={'4'} right />
+          <DataTableRow
+            hover
+            showCheckbox
+            selected={this.state.checkedOne}
+            onPressCheckBox={() =>
+                this.setState({ checkedOne: !this.state.checkedOne })
+            }>
+            <DataTableCell text={'Frozen yogurt'} borderRight flex={2} />
+            <DataTableCell text={'159'} right />
+            <DataTableCell text={'6.0'} right />
+            <DataTableCell text={'24'} right />
+            <DataTableCell text={'4'} right />
           </DataTableRow>
-          <DataTableRow>
+          <DataTableRow
+              hover
+              showCheckbox
+              selected={this.state.checkedTwo}
+              onPressCheckBox={() =>
+                  this.setState({ checkedTwo: !this.state.checkedTwo })
+              }>
               <DataTableCell text={'Ice Cream Sandwich'} borderRight flex={2} />
               <DataTableCell text={'237'} right />
               <DataTableCell text={'9.0'} right />
               <DataTableCell text={'37'} right />
               <DataTableCell text={'4.3'} right />
           </DataTableRow>
-          <DataTableRow>
+          <DataTableRow
+              hover
+              showCheckbox
+              selected={this.state.checkedThree}
+              onPressCheckBox={() =>
+                  this.setState({ checkedThree: !this.state.checkedThree })
+              }>
               <DataTableCell text={'Eclair'} borderRight flex={2} />
               <DataTableCell text={'262'} right />
               <DataTableCell text={'16.0'} right />
