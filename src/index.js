9ed80eb9b9d1ae73df920514eed619487d3d0b13
codypearce <codyapearce@gmail.com>
2019-05-28 04:12:51 -0700

    Change export

--- src/index.js
+++ src/index.js
@@ -1,112 +1,70 @@
 // ==== UTIL ====
-export { default as Hoverable } from './Utils/Hoverable/Hoverable';
-export { default as shadow } from './Utils/Shadow/shadow';
-export { default as Anchor } from './Utils/Anchor/Anchor';
+export Hoverable from './Utils/Hoverable/Hoverable';
+export shadow from './Utils/Shadow/shadow';
+export Anchor from './Utils/Anchor/Anchor';
 
 // ==== Components ====
-export { default as Appbar } from './Components/Appbar/Appbar';
-export {
-  default as AppbarBottom,
-} from './Components/AppbarBottom/AppbarBottom';
-export { default as Avatar } from './Components/Avatar/Avatar';
-export { default as Backdrop } from './Components/Backdrop/Backdrop';
-export { default as Badge } from './Components/Badge/Badge';
-export { default as Banner } from './Components/Banner/Banner';
-export {
-  default as BottomNavigation,
-} from './Components/BottomNavigation/BottomNavigation';
-export {
-  default as BottomNavigationItem,
-} from './Components/BottomNavigation/BottomNavigationItem/BottomNavigationItem';
-export { default as Button } from './Components/Button/Button';
-export { default as Card } from './Components/Card/Card';
-export {
-  default as CardActions,
-} from './Components/Card/CardActions/CardActions';
-export {
-  default as CardContent,
-} from './Components/Card/CardContent/CardContent';
-export { default as CardHeader } from './Components/Card/CardHeader/CardHeader';
-export { default as CardMedia } from './Components/Card/CardMedia/CardMedia';
-export { default as Checkbox } from './Components/Checkbox/Checkbox';
-export { default as Chip } from './Components/Chip/Chip';
-export { default as DataTable } from './Components/DataTable/DataTable';
-export {
-  default as DataTableHeader,
-} from './Components/DataTable/DataTableHeader/DataTableHeader';
-export {
-  default as DataTableCell,
-} from './Components/DataTable/DataTableCell/DataTableCell';
-export {
-  default as DataTablePagination,
-} from './Components/DataTable/DataTablePagination/DataTablePagination';
-export {
-  default as DataTableRow,
-} from './Components/DataTable/DataTableRow/DataTableRow';
+export Appbar from './Components/Appbar/Appbar';
+export AppbarBottom from './Components/AppbarBottom/AppbarBottom';
+export Avatar from './Components/Avatar/Avatar';
+export Backdrop from './Components/Backdrop/Backdrop';
+export Badge from './Components/Badge/Badge';
+export Banner from './Components/Banner/Banner';
+export BottomNavigation from './Components/BottomNavigation/BottomNavigation';
+export BottomNavigationItem from './Components/BottomNavigation/BottomNavigationItem/BottomNavigationItem';
+export Button from './Components/Button/Button';
+export Card from './Components/Card/Card';
+export CardActions from './Components/Card/CardActions/CardActions';
+export CardContent from './Components/Card/CardContent/CardContent';
+export CardHeader from './Components/Card/CardHeader/CardHeader';
+export CardMedia from './Components/Card/CardMedia/CardMedia';
+export Checkbox from './Components/Checkbox/Checkbox';
+export Chip from './Components/Chip/Chip';
+export DataTable from './Components/DataTable/DataTable';
+export DataTableHeader from './Components/DataTable/DataTableHeader/DataTableHeader';
+export DataTableCell from './Components/DataTable/DataTableCell/DataTableCell';
+export DataTablePagination from './Components/DataTable/DataTablePagination/DataTablePagination';
+export DataTableRow from './Components/DataTable/DataTableRow/DataTableRow';
 
-export { default as Dialog } from './Components/Dialog/Dialog';
+export Dialog from './Components/Dialog/Dialog';
 
-export { default as Drawer } from './Components/Drawer/Drawer';
-export {
-  default as DrawerHeader,
-} from './Components/Drawer/DrawerHeader/DrawerHeader';
-export {
-  default as DrawerItem,
-} from './Components/Drawer/DrawerItem/DrawerItem';
-export {
-  default as DrawerSection,
-} from './Components/Drawer/DrawerSection/DrawerSection';
-export {
-  default as DrawerBottom,
-} from './Components/DrawerBottom/DrawerBottom';
-export { default as Divider } from './Components/Divider/Divider';
-export { default as Fab } from './Components/Fab/Fab';
-export {
-  default as FabSpeedDial,
-} from './Components/Fab/FabSpeedDial/FabSpeedDial';
-export { default as Icon } from './Components/Icon/Icon';
-export { default as IconButton } from './Components/IconButton/IconButton';
-export { default as List } from './Components/List/List';
-export {
-  default as ListSection,
-} from './Components/List/ListSection/ListSection';
-export { default as ListExpand } from './Components/List/ListExpand/ListExpand';
-export { default as ListItem } from './Components/List/ListItem/ListItem';
+export Drawer from './Components/Drawer/Drawer';
+export DrawerHeader from './Components/Drawer/DrawerHeader/DrawerHeader';
+export DrawerItem from './Components/Drawer/DrawerItem/DrawerItem';
+export DrawerSection from './Components/Drawer/DrawerSection/DrawerSection';
+export DrawerBottom from './Components/DrawerBottom/DrawerBottom';
+export Divider from './Components/Divider/Divider';
+export Fab from './Components/Fab/Fab';
+export FabSpeedDial from './Components/Fab/FabSpeedDial/FabSpeedDial';
+export Icon from './Components/Icon/Icon';
+export IconButton from './Components/IconButton/IconButton';
+export List from './Components/List/List';
+export ListSection from './Components/List/ListSection/ListSection';
+export ListExpand from './Components/List/ListExpand/ListExpand';
+export ListItem from './Components/List/ListItem/ListItem';
 
-export { default as Menu } from './Components/Menu/Menu';
-export { default as MenuItem } from './Components/Menu/MenuItem/MenuItem';
-export { default as Paper } from './Components/Paper/Paper';
-export {
-  default as ProgressBar,
-} from './Components/Progress/ProgressBar/ProgressBar';
-export {
-  default as ProgressCircle,
-} from './Components/Progress/ProgressCircle/ProgressCircle';
-export { default as RadioButton } from './Components/RadioButton/RadioButton';
-export { default as Ripple } from './Components/Ripple/Ripple';
-export { default as Select } from './Components/Select/Select';
-export {
-  default as Searchfield,
-} from './Components/TextField/Searchfield/Searchfield';
-export { default as Searchbar } from './Components/Appbar/Searchbar/Searchbar';
-export { default as SheetBottom } from './Components/SheetBottom/SheetBottom';
-export { default as SheetSide } from './Components/SheetSide/SheetSide';
-export { default as Slider } from './Components/Slider/Slider';
-export { default as Snackbar } from './Components/Snackbar/Snackbar';
-export { default as Switch } from './Components/Switch/Switch';
-export { default as Tabs } from './Components/Tabs/Tabs';
-export { default as Tab } from './Components/Tabs/Tab/Tab';
-export { default as TextField } from './Components/TextField/TextField';
-export {
-  default as TextFieldHelperText,
-} from './Components/TextField/TextFieldHelperText/TextFieldHelperText';
-export {
-  default as ToggleButton,
-} from './Components/ToggleButton/ToggleButton';
-export {
-  default as ToggleButtonGroup,
-} from './Components/ToggleButton/ToggleButtonGroup/ToggleButtonGroup';
-export { default as Tooltip } from './Components/Tooltip/Tooltip';
+export Menu from './Components/Menu/Menu';
+export MenuItem from './Components/Menu/MenuItem/MenuItem';
+export Paper from './Components/Paper/Paper';
+export ProgressBar from './Components/Progress/ProgressBar/ProgressBar';
+export ProgressCircle from './Components/Progress/ProgressCircle/ProgressCircle';
+export RadioButton from './Components/RadioButton/RadioButton';
+export Ripple from './Components/Ripple/Ripple';
+export Select from './Components/Select/Select';
+export Searchfield from './Components/TextField/Searchfield/Searchfield';
+export Searchbar from './Components/Appbar/Searchbar/Searchbar';
+export SheetBottom from './Components/SheetBottom/SheetBottom';
+export SheetSide from './Components/SheetSide/SheetSide';
+export Slider from './Components/Slider/Slider';
+export Snackbar from './Components/Snackbar/Snackbar';
+export Switch from './Components/Switch/Switch';
+export Tabs from './Components/Tabs/Tabs';
+export Tab from './Components/Tabs/Tab/Tab';
+export TextField from './Components/TextField/TextField';
+export TextFieldHelperText from './Components/TextField/TextFieldHelperText/TextFieldHelperText';
+export ToggleButton from './Components/ToggleButton/ToggleButton';
+export ToggleButtonGroup from './Components/ToggleButton/ToggleButtonGroup/ToggleButtonGroup';
+export Tooltip from './Components/Tooltip/Tooltip';
 export {
   Heading,
   Subtitle,
@@ -116,10 +74,10 @@ export {
 } from './Components/Typography';
 
 // ==== Theme ====
-export { default as ThemeContext } from './Theme/ThemeContext';
-export { default as withTheme } from './Theme/withTheme';
-export { default as defaultTheme } from './Theme/defaultTheme';
+export ThemeContext from './Theme/ThemeContext';
+export withTheme from './Theme/withTheme';
+export defaultTheme from './Theme/defaultTheme';
 
 // ==== Core ====
-export { default as BreadProvider } from './Core/BreadProvider';
-export { default as Colors } from './Core/colors';
+export BreadProvider from './Core/BreadProvider';
+export Colors from './Core/colors';
