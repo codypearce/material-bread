f41956ac783e27445ee09a34c54c8d8278c2d97b
codypearce <codyapearce@gmail.com>
2019-05-27 20:42:34 -0700

    Add github button

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -5,6 +5,7 @@ import { LogoWithText, PlatformButton } from '@components';
 import browser from '../assets/browser.svg';
 import iphone from '../assets/iphone.svg';
 import imac from '../assets/imac.svg';
+import GitHubButton from 'react-github-btn';
 
 class Index extends Component {
   componentDidMount() {
@@ -16,6 +17,14 @@ class Index extends Component {
         <div style={{ marginTop: 40 }}>
           <LogoWithText subtitle style={{ width: '100%' }} />
         </div>
+
+        <GitHubButton
+          href="https://github.com/codypearce/material-bread"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star codypearce/material-bread on GitHub">
+          Star
+        </GitHubButton>
 
         <div className="Home__platformRow">
           <PlatformButton
