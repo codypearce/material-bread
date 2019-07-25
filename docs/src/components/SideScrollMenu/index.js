7100fd6ebb5c9f82d2cb757139e025ec9563f9a0
codypearce <codyapearce@gmail.com>
2019-07-24 23:21:13 -0700

    Create spyscroll slider

--- docs/src/components/SideScrollMenu/index.js
+++ docs/src/components/SideScrollMenu/index.js
@@ -12,6 +12,16 @@ class SideScrollMenu extends Component {
     items: PropTypes.array,
   };
 
+  state = {
+    slideTop: 0,
+    height: 60,
+  };
+
+  componentDidMount() {
+    const { items } = this.props;
+    this.setState({ height: items.length * 30 });
+  }
+
   formatLinkFromName(str) {
     str = str.replace(/\s+/g, '-').toLowerCase();
     return str.toString();
@@ -28,29 +38,49 @@ class SideScrollMenu extends Component {
   renderMenuItems(item, index) {
     const link = this.formatLinkFromName(item.name);
     return (
-      <li
-        key={index}
-        className={`SideMenu__ListItem ${
-          item.sub ? 'SideMenu__ListItem--sub' : ''
-        }`}>
-        <a className="SideMenu__Link" href={`#${link}`}>
+      <a className="SideMenu__Link" href={`#${link}`}>
+        <li
+          key={index}
+          className={`SideMenu__ListItem ${
+            item.sub ? 'SideMenu__ListItem--sub' : ''
+          }`}>
           {item.name}
-        </a>
-      </li>
+        </li>
+      </a>
     );
   }
+
+  animateSlideItem(id) {
+    const items = this.handleLinks();
+    const index = items.findIndex(item => item == id);
+    this.setState({ slideTop: 30 * index });
+  }
+
   render() {
     const { items } = this.props;
     if (typeof window == 'undefined') {
       return null;
     }
     return ReactDOM.createPortal(
-      <Scrollspy
-        items={this.handleLinks()}
-        currentClassName="is-current"
-        className="SideMenu">
-        {items && items.map((item, index) => this.renderMenuItems(item, index))}
-      </Scrollspy>,
+      <div className="SideMenuWrapper">
+        <li
+          className="sliding-wrapper"
+          style={{ height: `${this.state.height}px` }}
+        />
+        <li
+          className="sliding-border"
+          style={{ transform: `translateY(${this.state.slideTop}px)` }}
+        />
+
+        <Scrollspy
+          onUpdate={stuff => this.animateSlideItem(stuff.id)}
+          items={this.handleLinks()}
+          currentClassName="is-current"
+          className="SideMenu">
+          {items &&
+            items.map((item, index) => this.renderMenuItems(item, index))}
+        </Scrollspy>
+      </div>,
       document.body,
     );
   }
