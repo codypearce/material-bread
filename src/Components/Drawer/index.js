d1dfe3757db7347edfd11b77d634cf74c8e79378
Bruno <bruno.castro@codeminer42.com>
2019-09-03 23:46:42 -0300

    feat: add index.js to components

---
+++ src/Components/Drawer/index.js
@@ -0,0 +1,4 @@
+export { default } from './Drawer';
+export { default as DrawerHeader } from './DrawerHeader';
+export { default as DrawerItem } from './DrawerItem';
+export { default as DrawerSection } from './DrawerSection';
---
+++ src/Components/TextField/index.js
@@ -0,0 +1,2 @@
+export { default } from './TextField';
+export { default as TextFieldHelperText } from './TextFieldHelperText';
