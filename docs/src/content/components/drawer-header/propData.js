486f464b9a023a2e367236b4b8e6ff7b80b21e6f
Alexandre Choisy <alexandre.choisy@gmail.com>
2019-11-01 21:45:09 +0100

    Update DrawerHeader Doc propData with titleStyle and subtitleStyle

--- docs/src/content/components/drawer-header/propData.js
+++ docs/src/content/components/drawer-header/propData.js
@@ -3,7 +3,9 @@ const propData = [
   ['avatar', 'Shows above the title', 'node', ''],
   ['style', 'Styles container element', 'object', ''],
   ['subtitle', 'DrawerHeader subtitle shows below title', 'string', ''],
+  ['subtitleStyle', 'Style subtitle', 'object', ''],
   ['title', 'DrawerHeader title shows above subtitle', 'string', ''],
+  ['titleStyle', 'Style title', 'object', ''],
 ];
 
 export default createTableData(propData);
