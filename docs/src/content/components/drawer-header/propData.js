545893bf018a2728fab58cf9670f9b2e36ac3514
Alexandre Choisy <alexandre.choisy@gmail.com>
2019-11-02 22:34:01 +0100

    Doc change undo Drawer-header

--- docs/src/content/components/drawer-header/propData.js
+++ docs/src/content/components/drawer-header/propData.js
@@ -3,9 +3,7 @@ const propData = [
   ['avatar', 'Shows above the title', 'node', ''],
   ['style', 'Styles container element', 'object', ''],
   ['subtitle', 'DrawerHeader subtitle shows below title', 'string', ''],
-  ['subtitleStyle', 'Style subtitle', 'object', ''],
   ['title', 'DrawerHeader title shows above subtitle', 'string', ''],
-  ['titleStyle', 'Style title', 'object', ''],
 ];
 
 export default createTableData(propData);
