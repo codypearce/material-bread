a0176333c5bc53a2337bb5d4c30091dc4523d77e
Alexandre Choisy <alexandre.choisy@gmail.com>
2019-11-05 17:53:34 +0100

    Docs update

--- docs/src/content/components/drawer-item/propData.js
+++ docs/src/content/components/drawer-item/propData.js
@@ -3,6 +3,12 @@ const propData = [
   ['active', 'Toggles active state for drawer item', 'bool', ''],
   ['onPress', 'Callback when item is pressed', 'func', ''],
   ['icon', 'Icon name for icon shown on left', 'string', ''],
+  [
+    'iconComponent',
+    'One of the Icon components from react-native-vector-icons',
+    'function',
+    '',
+  ],
   ['style', 'Styles container element', 'object', ''],
   ['text', 'Text shown for item', 'string', ''],
 ];
--- docs/src/content/components/searchbar/propData.js
+++ docs/src/content/components/searchbar/propData.js
@@ -5,6 +5,21 @@ const propData = [
   ['onNavigation', 'Callback when navigation icon is Pressed', 'func', ''],
   ['style', 'Styles root element', 'object', ''],
   ['value', 'Search input', 'string', ''],
+  ['placeholder', 'Placeholder for input search', 'string', 'Search'],
+  ['navigationIcon', 'Navigation icon name', 'string', 'arrow-back'],
+  [
+    'navigationIconComponent',
+    'One of the Icon components from react-native-vector-icons',
+    'function',
+    '',
+  ],
+  ['closeIcon', 'Close icon name', 'string', 'close'],
+  [
+    'closeIconComponent',
+    'One of the Icon components from react-native-vector-icons',
+    'function',
+    '',
+  ],
 ];
 
 export default createTableData(propData);
