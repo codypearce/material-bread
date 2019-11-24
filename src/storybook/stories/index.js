1217f30ae566cc5e1f191d6e49b3521faa119995
codypearce <codyapearce@gmail.com>
2019-11-24 00:54:27 -0800

    Fix formatting

--- src/storybook/stories/index.js
+++ src/storybook/stories/index.js
@@ -2,76 +2,164 @@ export { default as Home } from './Home.stories.js';
 
 /* Components */
 export { default as Appbar } from '../../Components/Appbar/Appbar.stories';
-export { default as AppbarBottom } from '../../Components/AppbarBottom/AppbarBottom.stories.js';
+export {
+  default as AppbarBottom,
+} from '../../Components/AppbarBottom/AppbarBottom.stories.js';
 export { default as Avatar } from '../../Components/Avatar/Avatar.stories.js';
-export { default as Backdrop } from '../../Components/Backdrop/Backdrop.stories.js';
+export {
+  default as Backdrop,
+} from '../../Components/Backdrop/Backdrop.stories.js';
 export { default as Badge } from '../../Components/Badge/Badge.stories.js';
 export { default as Banner } from '../../Components/Banner/Banner.stories.js';
-export { default as BottomNavigation } from '../../Components/BottomNavigation/BottomNavigation.stories.js';
-export { default as BottomNavigationItem } from '../../Components/BottomNavigation/BottomNavigationItem/BottomNavigationItem.stories.js';
+export {
+  default as BottomNavigation,
+} from '../../Components/BottomNavigation/BottomNavigation.stories.js';
+export {
+  default as BottomNavigationItem,
+} from '../../Components/BottomNavigation/BottomNavigationItem/BottomNavigationItem.stories.js';
 
 export { default as Button } from '../../Components/Button/Button.stories.js';
-export { default as TextButton } from '../../Components/Button/TextButton/TextButton.stories.js';
-export { default as FlatButton } from '../../Components/Button/FlatButton/FlatButton.stories.js';
-export { default as OutlinedButton } from '../../Components/Button/OutlinedButton/OutlinedButton.stories.js';
-export { default as ContainedButton } from '../../Components/Button/ContainedButton/ContainedButton.stories.js';
+export {
+  default as TextButton,
+} from '../../Components/Button/TextButton/TextButton.stories.js';
+export {
+  default as FlatButton,
+} from '../../Components/Button/FlatButton/FlatButton.stories.js';
+export {
+  default as OutlinedButton,
+} from '../../Components/Button/OutlinedButton/OutlinedButton.stories.js';
+export {
+  default as ContainedButton,
+} from '../../Components/Button/ContainedButton/ContainedButton.stories.js';
 
 export { default as Card } from '../../Components/Card/Card.stories.js';
-export { default as Checkbox } from '../../Components/Checkbox/Checkbox.stories.js';
+export {
+  default as Checkbox,
+} from '../../Components/Checkbox/Checkbox.stories.js';
 export { default as Chip } from '../../Components/Chip/Chip.stories.js';
-export { default as DataTable } from '../../Components/DataTable/DataTable.stories.js';
-export { default as DataTableCell } from '../../Components/DataTable/DataTableCell/DataTableCell.stories.js';
-export { default as DataTableHeader } from '../../Components/DataTable/DataTableHeader/DataTableHeader.stories.js';
-export { default as DataTableRow } from '../../Components/DataTable/DataTableRow/DataTableRow.stories.js';
-export { default as DataTablePagination } from '../../Components/DataTable/DataTablePagination/DataTablePagination.stories.js';
+export {
+  default as DataTable,
+} from '../../Components/DataTable/DataTable.stories.js';
+export {
+  default as DataTableCell,
+} from '../../Components/DataTable/DataTableCell/DataTableCell.stories.js';
+export {
+  default as DataTableHeader,
+} from '../../Components/DataTable/DataTableHeader/DataTableHeader.stories.js';
+export {
+  default as DataTableRow,
+} from '../../Components/DataTable/DataTableRow/DataTableRow.stories.js';
+export {
+  default as DataTablePagination,
+} from '../../Components/DataTable/DataTablePagination/DataTablePagination.stories.js';
 export { default as Dialog } from '../../Components/Dialog/Dialog.stories.js';
 
 export { default as Drawer } from '../../Components/Drawer/Drawer.stories.js';
-export { default as DrawerHeader } from '../../Components/Drawer/DrawerHeader/DrawerHeader.stories.js';
-export { default as DrawerSection } from '../../Components/Drawer/DrawerSection/DrawerSection.stories.js';
-export { default as DrawerItem } from '../../Components/Drawer/DrawerItem/DrawerItem.stories.js';
-export { default as DrawerBottom } from '../../Components/DrawerBottom/DrawerBottom.stories.js';
-export { default as Divider } from '../../Components/Divider/Divider.stories.js';
+export {
+  default as DrawerHeader,
+} from '../../Components/Drawer/DrawerHeader/DrawerHeader.stories.js';
+export {
+  default as DrawerSection,
+} from '../../Components/Drawer/DrawerSection/DrawerSection.stories.js';
+export {
+  default as DrawerItem,
+} from '../../Components/Drawer/DrawerItem/DrawerItem.stories.js';
+export {
+  default as DrawerBottom,
+} from '../../Components/DrawerBottom/DrawerBottom.stories.js';
+export {
+  default as Divider,
+} from '../../Components/Divider/Divider.stories.js';
 export { default as Fab } from '../../Components/Fab/Fab.stories.js';
-export { default as FabSpeedDial } from '../../Components/Fab/FabSpeedDial/FabSpeedDial.stories.js';
+export {
+  default as FabSpeedDial,
+} from '../../Components/Fab/FabSpeedDial/FabSpeedDial.stories.js';
 export { default as Icons } from '../../Components/Icon/Icons.stories.js';
-export { default as IconButton } from '../../Components/IconButton/IconButton.stories.js';
+export {
+  default as IconButton,
+} from '../../Components/IconButton/IconButton.stories.js';
 export { default as List } from '../../Components/List/List.stories.js';
-export { default as ListExpand } from '../../Components/List/ListExpand/ListExpand.stories.js';
-export { default as ListItem } from '../../Components/List/ListItem/ListItem.stories.js';
-export { default as ListSection } from '../../Components/List/ListSection/ListSection.stories.js';
+export {
+  default as ListExpand,
+} from '../../Components/List/ListExpand/ListExpand.stories.js';
+export {
+  default as ListItem,
+} from '../../Components/List/ListItem/ListItem.stories.js';
+export {
+  default as ListSection,
+} from '../../Components/List/ListSection/ListSection.stories.js';
 
 export { default as Menu } from '../../Components/Menu/Menu.stories.js';
-export { default as MenuItem } from '../../Components/Menu/MenuItem/MenuItem.stories.js';
+export {
+  default as MenuItem,
+} from '../../Components/Menu/MenuItem/MenuItem.stories.js';
 export { default as Paper } from '../../Components/Paper/Paper.stories.js';
-export { default as ProgressBar } from '../../Components/Progress/ProgressBar/ProgressBar.stories.js';
-export { default as ProgressCircle } from '../../Components/Progress/ProgressCircle/ProgressCircle.stories.js';
-export { default as RadioButton } from '../../Components/RadioButton/RadioButton.stories.js';
+export {
+  default as ProgressBar,
+} from '../../Components/Progress/ProgressBar/ProgressBar.stories.js';
+export {
+  default as ProgressCircle,
+} from '../../Components/Progress/ProgressCircle/ProgressCircle.stories.js';
+export {
+  default as RadioButton,
+} from '../../Components/RadioButton/RadioButton.stories.js';
 export { default as Ripple } from '../../Components/Ripple/Ripple.stories.js';
-export { default as Searchbar } from '../../Components/Appbar/Searchbar/Searchbar.stories.js';
-export { default as Searchfield } from '../../Components/TextField/Searchfield/Searchfield.stories.js';
-export { default as SelectFilled } from '../../Components/Select/SelectFilled/SelectFilled.stories.js';
-export { default as SelectFlat } from '../../Components/Select/SelectFlat/SelectFlat.stories.js';
-export { default as SelectOutlined } from '../../Components/Select/SelectOutlined/SelectOutlined.stories.js';
+export {
+  default as Searchbar,
+} from '../../Components/Appbar/Searchbar/Searchbar.stories.js';
+export {
+  default as Searchfield,
+} from '../../Components/TextField/Searchfield/Searchfield.stories.js';
+export {
+  default as SelectFilled,
+} from '../../Components/Select/SelectFilled/SelectFilled.stories.js';
+export {
+  default as SelectFlat,
+} from '../../Components/Select/SelectFlat/SelectFlat.stories.js';
+export {
+  default as SelectOutlined,
+} from '../../Components/Select/SelectOutlined/SelectOutlined.stories.js';
 
-export { default as SheetBottom } from '../../Components/SheetBottom/SheetBottom.stories.js';
-export { default as SheetSide } from '../../Components/SheetSide/SheetSide.stories.js';
+export {
+  default as SheetBottom,
+} from '../../Components/SheetBottom/SheetBottom.stories.js';
+export {
+  default as SheetSide,
+} from '../../Components/SheetSide/SheetSide.stories.js';
 export { default as Slider } from '../../Components/Slider/Slider.stories.js';
-export { default as Snackbar } from '../../Components/Snackbar/Snackbar.stories.js';
-export { default as SwipeNav } from '../../Components/SwipeNav/SwipeNav.stories.js';
+export {
+  default as Snackbar,
+} from '../../Components/Snackbar/Snackbar.stories.js';
+export {
+  default as SwipeNav,
+} from '../../Components/SwipeNav/SwipeNav.stories.js';
 
 export { default as Switch } from '../../Components/Switch/Switch.stories.js';
 export { default as Tab } from '../../Components/Tabs/Tab/Tab.stories.js';
 export { default as Tabs } from '../../Components/Tabs/Tabs.stories.js';
-export { default as TextFieldFilled } from '../../Components/TextField/TextFieldFilled/TextFieldFilled.stories.js';
-export { default as TextFieldFlat } from '../../Components/TextField/TextFieldFlat/TextFieldFlat.stories.js';
-export { default as TextFieldOutline } from '../../Components/TextField/TextFieldOutline/TextFieldOutline.stories.js';
+export {
+  default as TextFieldFilled,
+} from '../../Components/TextField/TextFieldFilled/TextFieldFilled.stories.js';
+export {
+  default as TextFieldFlat,
+} from '../../Components/TextField/TextFieldFlat/TextFieldFlat.stories.js';
+export {
+  default as TextFieldOutline,
+} from '../../Components/TextField/TextFieldOutline/TextFieldOutline.stories.js';
 
-export { default as ToggleButton } from '../../Components/ToggleButton/ToggleButton.stories.js';
-export { default as Tooltip } from '../../Components/Tooltip/Tooltip.stories.js';
-export { default as Typography } from '../../Components/Typography/Typography.stories.js';
+export {
+  default as ToggleButton,
+} from '../../Components/ToggleButton/ToggleButton.stories.js';
+export {
+  default as Tooltip,
+} from '../../Components/Tooltip/Tooltip.stories.js';
+export {
+  default as Typography,
+} from '../../Components/Typography/Typography.stories.js';
 
 /* Utils */
 export { default as Anchor } from '../../Utils/Anchor/Anchor.stories.js';
-export { default as Hoverable } from '../../Utils/Hoverable/Hoverable.stories.js';
+export {
+  default as Hoverable,
+} from '../../Utils/Hoverable/Hoverable.stories.js';
 export { default as Shadows } from '../../Utils/Shadow/Shadow.stories.js';
