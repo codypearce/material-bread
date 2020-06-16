53d6e3dc26d6b94f248eaee3135009e3c233bff9
Jose Ramirez <joseramirez@Jose-Ramirez-MacBook-Pro.local>
2020-06-16 10:19:45 -0500

    MAT-25 Add full card story

--- src/Components/Card/CardActions/CardActions.styles.js
+++ src/Components/Card/CardActions/CardActions.styles.js
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     padding: 24,
+    paddingTop: 16,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
